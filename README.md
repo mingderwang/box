d10d125d1129acbb8361a41baefabdcc9ee9e9a9
Ming-der Wang <mingderwang@gmail.com>
2020-12-20 22:15:25 +0800

    pure test without helper

---
+++ README.md
@@ -0,0 +1,2 @@
+# test
+https://docs.openzeppelin.com/learn/writing-automated-tests#setting-up-a-testing-environment
---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+// scripts/index.js
+async function main() {
+  // Our code will go here
+// Retrieve accounts from the local node
+const accounts = await ethers.provider.listAccounts();
+//console.log(accounts);
+
+// Set up an ethers contract, representing our deployed Box instance
+const address = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
+const Box = await ethers.getContractFactory("Box");
+const box = await Box.attach(address);
+
+// Send a transaction to store() a new value in the Box
+await box.store(123);
+
+// Call the retrieve() function of the deployed Box contract
+value = await box.retrieve();
+console.log("Box value is", value.toString());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
