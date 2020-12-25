431fae38c7858d00171a7c8208112d1bfba08c82
Ming-der Wang <mingderwang@gmail.com>
2020-12-25 13:37:46 +0800

    open hardhat.config.js file, no secret here

---
+++ hardhat.config.js
@@ -0,0 +1,37 @@
+// hardhat.config.js
+require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-truffle5");
+require("hardhat-deploy-ethers");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+const { privateKey, infuraProjectId, mnemonic, etherscanApiKey } = require('./secrets.json');
+
+// task action function receives the Hardhat Runtime Environment as second argument
+task(
+  "blockNumber",
+  "Prints the current block number",
+  async (_, { ethers }) => {
+    await ethers.provider.getBlockNumber().then((blockNumber) => {
+      console.log("Current block number: " + blockNumber);
+    });
+  }
+);
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat: {
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${infuraProjectId}`,
+      accounts: [privateKey]
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${infuraProjectId}`,
+      accounts: [privateKey]
+    },
+  },
+  solidity: "0.6.12",
+};
